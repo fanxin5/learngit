3e8f32ab5296626dcb40c0945b7e90cb1e961355
cqc <2954579687@qq.com>
2023-09-22 17:31:06 +0800

    wrote a git_code01 file

---
+++ git_code01.cpp
@@ -0,0 +1,13 @@
+﻿// git_code01.cpp : 此文件包含 "main" 函数。程序执行将在此处开始并结束。
+//
+
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    cout << "this is a simple code!" << endl;
+    cout << "studying using Git" << endl;
+
+    return 0;
+}
